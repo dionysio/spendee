c4752c7c147665375d7c4c99318b654a18513ffc
dio <contact@dionysio.com>
2020-04-17 18:03:54 +0700

    updated spendee import/setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="spendee-api",
+    name="spendee",
     version="0.9.9",
     author="Dionyz Lazar",
     author_email="dio@dionysio.com",
