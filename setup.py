881164d31dc656ef577100f1a644e89ae077fe1e
dio <contact@dionysio.com>
2020-04-17 17:57:34 +0700

    added setup stuff for pip

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/dionysio/spendee",
     packages=setuptools.find_packages(),
+    install_requires=['requests'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
